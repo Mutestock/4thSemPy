b23d04b1a923a9434e5a171eb4b14dc35bd6c1d5
unknown <doublenegative@protonmail.com>
2020-02-08 19:51:52 +0100

    deleted some unnecessary info

--- week2/exercise02/utils_test.py
+++ week2/exercise02/utils_test.py
@@ -6,8 +6,13 @@ if __name__ == "__main__":
     exeLogic.first_func(os.path.dirname(os.path.realpath(__file__)))
     exeLogic.second_func(os.path.dirname(os.path.realpath(__file__)))
 
-    third_func_file_list = [
-        "C:/Users/Henning/Desktop/FoxyBoxesMainFolder/playlistOrganizerProjectNotes.txt", "C:/Users/Henning/Desktop/FoxyBoxesMainFolder/Foxy Boxes/backup/pre_restplus_rebuild/amb/src/ambAPI/schema/playlist_schema.py"]
+    # I could step back in the repos and make it test stuff automatically, but I don't know what's on the user's computer and therefore I don't want to mess around with it too much
+    # Additionally, I don't want to show people anything about my directories
+    # and finally anyone else pulling the program and trying to run it wouldn't be able to test it, since they don't have the same directories as I do
+    # Hence the list is empty
+    # Here's an example of what an input variable could look like
+    # "C:/Users/JohnDoe/Desktop/assignments/howToMakeABigCake.txt"
+    third_func_file_list = []
     exeLogic.third_func(third_func_file_list)
 
     fourth_func_file_list = [os.path.dirname(os.path.realpath(
